94fa32f7673e7298f4d745d4809112274c2ec26b
Blanca Hernandez <blanca_hdz23@hotmail.com>
2019-08-05 15:23:19 -0500

    Primer scrip

---
+++ Clase 0/clase_0.R
@@ -0,0 +1,23 @@
+#Blanca Hernández
+#05/08/2019
+#clase 0
+
+# #pasos básicos ----------------------------------------------------------
+
+2+2
+a <- 2
+
+a + a
+a + 5
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+diametro
+#Medidas de tendencia central
+
+mean(diametro)
+median(diametro)
+
+#Medidas de dispersión
+sd(diametro)
+var(diametro)
