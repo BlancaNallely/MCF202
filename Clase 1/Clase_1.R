26cd81d7e487124c06dde5d1cc472c9a4b8c3fd5
Blanca Hernandez <blanca_hdz23@hotmail.com>
2019-08-06 14:41:13 -0500

    Clase_1

--- Clase 1/Clase_1.R
+++ Clase 1/Clase_1.R
@@ -2,6 +2,10 @@
 # 06/08/2019
 # Clase 1
 
+#ESTADISTICA DESCRIPTIVA
+
+#El conjunto de datos podemos ingresarlos en el lenguaje R dela siguiente forma:
+  
 dbh <- c(16.5, 25.3, 22.1, 17.2, 16.1, 8.1, 34.3, 5.4, 5.7, 11.2, 24.1, 
          14.5, 7.7, 15.6, 15.9, 10, 17.5, 20.5, 7.8, 27.3, 9.7, 6.5, 
          23.4, 8.2, 28.5, 10.4, 11.5, 14.3, 17.2, 16.8)
