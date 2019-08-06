65250a69d87afd13a9d3d7d4e552f5c07ce073e0
Blanca Hernandez <blanca_hdz23@hotmail.com>
2019-08-06 15:10:04 -0500

    Clase_2

---
+++ Clase_2/vivero.R
@@ -0,0 +1,11 @@
+#Blanca Hernández
+#05/08/2019
+#clase 2
+
+#BASE DE DATOS VIVEROS
+
+
+# Importar Datos Vivero ---------------------------------------------------
+
+vivero <- read.csv("C:/MCF202-2019/MCF202/Datos/Tvivero.csv", header = T)
+summary(vivero)
