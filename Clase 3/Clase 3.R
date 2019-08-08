aee293155a415da492cb5cabc545a09efb4a2237
Blanca Hernandez <blanca_hdz23@hotmail.com>
2019-08-08 12:18:06 -0500

    Clase 3

--- Clase 3/Clase 3.R
+++ Clase 3/Clase 3.R
@@ -2,6 +2,8 @@
 #07/08/2018
 #Clase 3
 
+#EJERCICIO 1
+#Procedimiento general para la prueba de t independientes
 grupo <- gl(2,12, labels = c("Fotografia", "Araña"))
 Ansiedad <- c(30, 35, 45, 40, 50, 35, 55, 25, 30, 45, 40, 50, 40, 35, 50, 55,
               65, 55, 50, 35, 30, 50, 60, 39)
@@ -9,18 +11,25 @@ summary(grupo)
 
 Datos <- data.frame(grupo,Ansiedad)
 head(Datos)
+
+#Diferencias que existen entre los grupos
 #Analisis de muestras independientes
 boxplot(Datos$Ansiedad ~ Datos$grupo, col="lightgreen", ylab="Nivel de ansiedad")
-
 
 #La media de grupos
 tapply(Datos$Ansiedad, Datos$grupo, mean)
 
 mean(Datos$Ansiedad)
 
+#Describa la hipotesis nula y la alternativa
+#H0= No existe una diferencia significativa entre los niveles de ansiedad
+#de los dos grupos.
 
-#Describir la hipotesis nula y alternativa
+#H1= si existen diferencias significativas entre los grupos.
+
 shapiro.test(Datos$Ansiedad)
+
+bartlett.test(Datos$Ansiedad, Datos$grupo)
 
 #Tienen una distribucion normal ya que p (0.4977)es mayor al alfa (0.05)
 
@@ -30,13 +39,16 @@ by(Datos$Ansiedad, Datos$grupo, stat.desc, basic= FALSE, norm= TRUE)
 
 #Aplicar la prueba de t
 
-gr.t
+gr.t <-t.test(Datos$Ansiedad ~ Datos$grupo, var.equal= TRUE)
+
+n<- length(grupo)
 
 
 # Ejercicio 2 -------------------------------------------------------------
 
-#Hipotesis nula: la media que tiene el consumidor es igual al que dice la empresa (la media es igual a 80)
-#La hipotesis alternativa:que la media del consumidor es menor a 80 que dice el vendedor
+#Hipotesis nula (H0): la media que tiene el consumidor es igual al que dice la empresa (la media es igual a 80)
+
+#La hipotesis alternativa (H1):que la media del consumidor es menor a 80 que dice el vendedor
 
 
 costal <- c(87.7, 80.01, 77.28, 78.76, 81.52, 74.2, 80.71, 79.5, 77.87, 81.94, 80.7,
@@ -44,16 +56,18 @@ costal <- c(87.7, 80.01, 77.28, 78.76, 81.52, 74.2, 80.71, 79.5, 77.87, 81.94, 8
             73.59, 77.92, 77.18, 79.83, 81.23, 79.28, 78.44, 79.01, 80.47, 76.23,
             78.89, 77.14, 69.94, 78.54, 79.7, 82.45, 77.29, 75.52, 77.21, 75.99,
             81.94, 80.41, 77.7)
+
 summary(costal)
 
 #Determinar el numero de observaciones
 n <- length(costal)
+n
 
 #Determinar la media
-mean(costal)
 
-costal.media <- mean(costal)
-costal.media
+costa.media <- mean(costal)
+costa.media
+
 #Sacar la desviacion estandar
 
 costa.sd <- sd(costal)
@@ -61,14 +75,15 @@ costa.sd
 
 #Formula para obtner el valor de t
 #Sacar el valor de t
-costa.se <- costa.sd/sqrt(n)
+costa.se <- costa.sd/ sqrt(n)
+costa.se
 
 #Valor de T
 costa.T <- (costa.media - 80)/costa.se
+costa.T
 
 #Calcular el valor de P
 pt(costa.T, df = n-1)
 
-t.test(costal, mu=80, alternative = "less")
-
-
+#Procedimiento en R
+t.test(costal, mu=80, alternative = "less") 
