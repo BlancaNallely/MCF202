ccb7d38f1536428fea0d23487ad448de08797b1d
Blanca Hernandez <blanca_hdz23@hotmail.com>
2019-08-08 12:52:26 -0500

    Clase 0

--- Clase 0/clase_0.R
+++ Clase 0/clase_0.R
@@ -10,9 +10,13 @@ a <- 2
 a + a
 a + 5
 
+
+# Importar datos ----------------------------------------------------------
+
 diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
               11.8, 14.3, 12.5)
 diametro
+
 #Medidas de tendencia central
 
 mean(diametro)
@@ -23,39 +27,39 @@ sd(diametro)
 var(diametro)
 
 
-
-# Gráficas ----------------------------------------------------------------
+# Graficas ----------------------------------------------------------------
 
 boxplot(diametro, horizontal = TRUE, col = "lightblue", main="Diametro",
         xlab="D (cm)")
 
 
-# Importado excel actvividad 1---------------------------------------------------------
+# Importar datos ----------------------------------------------------------
 
-DB_alturas <- read.csv("c:/MCF202-2019/Datos/alturas.csv", header = T)
-head(DB_alturas)
-boxplot(DB_alturas$crecimiento)
-boxplot(DB_alturas$crecimiento ~ DB_alturas$tratamiento,
-        col= "ligthgreen",
-        xlab= "Tratamientos",
-        ylab= "crecimiento (cm)",
-        main= "Efectos del fertilizante")
+BD_alturas <-read.csv("c:/MCF202-2019/MCF202/Datos/Alturas.csv")
 
-mean(DB_alturas$crecimiento)
+#Graficas
+boxplot(BD_alturas$crecimiento)
+boxplot(BD_alturas$crecimiento ~ BD_alturas$tratamiento,
+        col= "lightgreen",
+        xlab= "tratamiento",
+        ylab= "crecimiento",
+        main= "Efectos de fertilizante" )
 
+mean (BD_alturas$crecimiento)
 
 #Restricciones ----------------------------------------------------------
-
-sum(DB_alturas$crecimiento < mean(DB_alturas$crecimiento))
+sum(BD_alturas$crecimiento < mean(BD_alturas$crecimiento))
 
 #Excluir el Tratamiento A
-Trat_A <- DB_alturas[!(DB_altura$tratamiento == "TA"),]
 
-mean(Trat_A$crecimiento)
+TratA <- BD_alturas[!(BD_alturas$tratamiento == "TA"),]
+
+mean(TratA$crecimiento)
 
 
 # Submuestra --------------------------------------------------------------
 
-T.mean <- subset(DB_alturas, crecimiento >= mean(DB_alturas$crecimiento)
-boxplot(T.mean$crecimiento ~ T.mean$tratamiento)                 
+T.mean <- subset(BD_alturas,crecimiento >= mean(BD_alturas$crecimiento))
+boxplot(T.mean$crecimiento ~ T.mean$tratamiento)
+                 
 
